6ff232437f92f61183b56c2e643c9d3860ae123f
muhammad1abbasi <159998253+muhammad1abbasi@users.noreply.github.com>
2025-03-24 21:32:31 +0500

    Update BMI_Calculator.py

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -6,12 +6,12 @@ st.title("BMI Calculator")
 height = st.slider("Enter your height (in cm):", 100, 250, 175)
 weight = st.slider("Enter your weight (in kg):", 40, 200, 70)
 
-bmi = weight / ((height/100) ** 2)
+bmi = weight / ((height / 100) ** 2)
 
 st.write(f"Your BMI is {bmi:.2f}")
 
 st.write(f"### BMI Categories ###")
-st.wirte("- Underweight: BMI less then 18.5")
-st.write("- Normal wight: BMI between 18.5 and 24.9")
-st.write("- Overweight: BMI between 2 and 29.9 ")
+st.write("- Underweight: BMI less than 18.5") `
+st.write("- Normal weight: BMI between 18.5 and 24.9")  
+st.write("- Overweight: BMI between 25 and 29.9") 
 st.write("- Obesity: BMI 30 or greater")
